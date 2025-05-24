56f09487d6debedaa4330199bbdffca839fe9b83
jasee8386 <isnu8386@gmail.com>
2025-05-24 18:44:51 +0530

    react router dome downgraded

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Wrap your App */}
-      <App />
-    </BrowserRouter>
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
